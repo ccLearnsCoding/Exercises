0e48c8f1cc14289f0b86bce601c65877f680aba3
Caleb Chan Yee Hang <caleb.chan.yee.hang@avanade.com>
2023-08-28 16:05:33 +0800

    initial commit

---
+++ CSharpLabsDay1/HelloMoon/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter name");
+        string name = Console.ReadLine();
+        DisplayMessage(name);
+    }
+    static void DisplayMessage(string name)
+    {
+        Console.WriteLine($"Welcome {name}");
+    }
+}
---
+++ Exercises/Exercises/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Initialize variables
+        string name = "Caleb Chan";
+        string streetAddress = "123 Main St";
+        int apartmentNumber = 456;
+        string city = "Anytown";
+        string state = "CA";
+
+        // Display the address label
+        Console.WriteLine($"{name}");
+        Console.WriteLine($"{streetAddress} Apt #{apartmentNumber}");
+        Console.WriteLine($"{city}, {state}");
+    }
+}
