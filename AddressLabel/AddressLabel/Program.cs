efdc2123d4245cb2b84d1faa8aa7ae1467cf29a5
Caleb Chan Yee Hang <caleb.chan.yee.hang@avanade.com>
2023-08-29 14:37:58 +0800

    add AdressLabel project

---
+++ AddressLabel/AddressLabel/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Initialize variables
+        string name = "John Doe";
+        string streetAddress = "MockStreetAddress";
+        int apartmentNumber = 123;
+        string city = "MockCity";
+        string state = "MockState";
+
+        // Display the address label
+        Console.WriteLine($"{name}");
+        Console.WriteLine($"{streetAddress} Apt #{apartmentNumber}");
+        Console.WriteLine($"{city}, {state}");
+    }
+}
