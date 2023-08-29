639639ba2270b01778df5161a522d5a5671b18ce
Caleb Chan Yee Hang <caleb.chan.yee.hang@avanade.com>
2023-08-29 15:17:48 +0800

    add DataScrubbing and StudentAdvising projects

---
+++ DataScrubbing/DataScrubbing/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    public static void ScrubPhone(ref string phoneNumber)
+    {
+        phoneNumber = phoneNumber.Trim(); // Removes leading and trailing spaces
+    }
+
+    static void Main(string[] args)
+    {
+        do
+        {
+            Console.Write("Enter a phone number: ");
+            string phoneNumber = Console.ReadLine();
+
+            // Call the ScrubPhone method to remove leading and trailing spaces
+            ScrubPhone(ref phoneNumber);
+
+            Console.WriteLine($"Scrubbed phone number: {phoneNumber}");
+
+            Console.Write("Do you want to enter another phone number? (y/n): ");
+            string userInput = Console.ReadLine();
+
+            if (userInput.ToLower() != "y")
+                break;
+
+        } while (true);
+
+        Console.WriteLine("Goodbye!");
+    }
+}
---
+++ StudentAdvising/StudentAdvising/Program.cs
@@ -0,0 +1,56 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        do
+        {
+            GetData(out string studentName, out string majorCode, out string classification);
+
+            string advisingLocation = GetAdvisingLocation(majorCode, classification);
+
+            Console.WriteLine($"Advising for {majorCode} {classification} majors: {advisingLocation}");
+
+            Console.Write("Do you want to enter another? (y/n): ");
+        } while (Console.ReadLine().Trim().ToUpper() == "Y");
+    }
+
+    static void GetData(out string studentName, out string majorCode, out string classification)
+    {
+        Console.Write("What is the student's name? ");
+        studentName = Console.ReadLine();
+
+        Console.Write("What is the student's major? ");
+        majorCode = Console.ReadLine().ToUpper();
+
+        Console.Write("What is the student's classification? ");
+        classification = Console.ReadLine().Trim();
+    }
+
+    static string GetAdvisingLocation(string majorCode, string classification)
+    {
+        string advisingLocation;
+
+        switch (majorCode)
+        {
+            case "BIOL":
+                advisingLocation = (classification == "Freshman" || classification == "Sophomore") ? "Science Bldg, Room 310" : "Science Bldg, Room 311";
+                break;
+            case "CSCI":
+                advisingLocation = "Sheppard Hall, Room 314";
+                break;
+            case "ENG":
+                advisingLocation = (classification == "Freshman") ? "Kerr Hall, Room 201" : "Kerr Hall, Room 312";
+                break;
+            case "HIST":
+                advisingLocation = "Kerr Hall, Room 114";
+                break;
+            case "MKT":
+                advisingLocation = (classification != "Senior") ? "Westly Hall, Room 310" : "Westly Hall, Room 313";
+                break;
+            default:
+                advisingLocation = "Unknown major code";
+                break;
+        }
+        return advisingLocation;
+    }
+}
