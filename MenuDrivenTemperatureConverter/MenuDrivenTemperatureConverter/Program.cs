524112a1edf92a88baded4f281c2e54eee30c733
Caleb Chan Yee Hang <caleb.chan.yee.hang@avanade.com>
2023-08-29 14:19:14 +0800

    Refactoring

---
+++ MenuDrivenTemperatureConverter/MenuDrivenTemperatureConverter/Program.cs
@@ -0,0 +1,68 @@
+﻿internal class Program
+{
+    private static void Main()
+    {
+        string choice;
+        do
+        {
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("1. CtoF (Convert Celsius to Fahrenheit)");
+            Console.WriteLine("2. FtoC (Convert Fahrenheit to Celsius)");
+            Console.WriteLine("3. Quit");
+
+            choice = Console.ReadLine();
+
+            switch (choice.ToLower())
+            {
+                case "1":
+                case "ctof":
+                    double convertedFahrenheit;
+                    Console.Write("Enter a Celsius temperature: ");
+                    if (double.TryParse(Console.ReadLine(), out double celsius))
+                    {
+                        convertedFahrenheit = ConvertCtoF(celsius);
+                        Console.WriteLine($"{celsius} degrees in Celsius is {convertedFahrenheit} degrees in Fahrenheit");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Please enter a valid number.");
+                    }
+                    break;
+
+                case "2":
+                case "ftoc":
+                    double convertedCelcius;
+                    Console.Write("Enter a Fahrenheit temperature: ");
+                    if (double.TryParse(Console.ReadLine(), out double fahrenheit))
+                    {
+                        convertedCelcius = ConvertFtoC(fahrenheit);
+                        Console.WriteLine($"{fahrenheit} degrees in Fahrenheit is {convertedCelcius} degrees in Celsius");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Please enter a valid number.");
+                    }
+                    break;
+
+                case "3":
+                case "quit":
+                    Console.WriteLine("Goodbye!");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice. Please enter a valid option.");
+                    break;
+            }
+
+        } while (choice.ToLower() != "3" && choice.ToLower() != "quit");
+    }
+    static double ConvertCtoF(double celsius)
+    {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    static double ConvertFtoC(double fahrenheit)
+    {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+}
--- MortgageCalculator/MortgageCalculator/Program.cs
+++ MortgageCalculator/MortgageCalculator/Program.cs
@@ -4,40 +4,69 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        // Get the loan amount from the user
-        Console.Write("How much are you borrowing? ");
-        if (!double.TryParse(Console.ReadLine(), out double loanAmount))
+        while (true)
         {
-            Console.WriteLine("Invalid input for loan amount. Please enter a valid number.");
-            return;
+            try
+            {
+                double loanAmount, interestRate;
+                int loanDurationYears;
+
+                // Get the loan amount from the user
+                Console.Write("How much are you borrowing? ");
+                if (!double.TryParse(Console.ReadLine(), out loanAmount) || loanAmount <= 0)
+                {
+                    Console.WriteLine("ERROR: Invalid input for loan amount. Please enter a valid positive number.");
+                    if (!GetYesNoResponse("Do you want to get information for another loan (Y/N)? "))
+                        break; // Exit the loop if the user's response is not 'Y'
+                    continue; // Continue to the next iteration of the loop
+                }
+
+                // Get the interest rate from the user
+                Console.Write("What is your interest rate? ");
+                if (!double.TryParse(Console.ReadLine(), out interestRate) || interestRate <= 0)
+                {
+                    Console.WriteLine("ERROR: Invalid input for interest rate. Please enter a valid positive number.");
+                    if (!GetYesNoResponse("Do you want to get information for another loan (Y/N)? "))
+                        break; // Exit the loop if the user's response is not 'Y'
+                    continue; // Continue to the next iteration of the loop
+                }
+
+                // Get the loan duration in years from the user
+                Console.Write("How long is your loan (in years)? ");
+                if (!int.TryParse(Console.ReadLine(), out loanDurationYears) || loanDurationYears <= 0)
+                {
+                    Console.WriteLine("ERROR: Invalid input for loan duration. Please enter a valid positive number of years.");
+                    if (!GetYesNoResponse("Do you want to get information for another loan (Y/N)? "))
+                        break; // Exit the loop if the user's response is not 'Y'
+                    continue; // Continue to the next iteration of the loop
+                }
+
+                // Calculate the monthly payment
+                double monthlyInterestRate = interestRate / 1200; // Convert annual interest rate to monthly
+                int numberOfPayments = loanDurationYears * 12;
+                double monthlyPayment = (loanAmount * monthlyInterestRate) / (1 - Math.Pow(1 + monthlyInterestRate, -numberOfPayments));
+                double totalPayment = monthlyPayment * numberOfPayments;
+                double totalInterest = totalPayment - loanAmount;
+
+                // Display the result
+                Console.WriteLine($"Your estimated payment is {monthlyPayment:C}");
+                Console.WriteLine($"You paid {totalPayment:C} over the life of the loan");
+                Console.WriteLine($"Your total interest cost for the loan was {totalInterest:C}");
+
+                // Ask if the user wants to get information for another loan
+                if (!GetYesNoResponse("Do you want to get information for another loan (Y/N)? "))
+                    break; // Exit the loop if the user's response is not 'Y'
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An unexpected error occurred: {ex.Message}");
+            }
         }
-
-        // Get the interest rate from the user
-        Console.Write("What is your interest rate? ");
-        if (!double.TryParse(Console.ReadLine(), out double interestRate))
-        {
-            Console.WriteLine("Invalid input for interest rate. Please enter a valid number.");
-            return;
-        }
-
-        // Get the loan duration in years from the user
-        Console.Write("How long is your loan (in years)? ");
-        if (!int.TryParse(Console.ReadLine(), out int loanDurationYears))
-        {
-            Console.WriteLine("Invalid input for loan duration. Please enter a valid number of years.");
-            return;
-        }
-
-        // Calculate the monthly payment
-        double monthlyInterestRate = interestRate / 1200; // Convert annual interest rate to monthly
-        int numberOfPayments = loanDurationYears * 12;
-        double monthlyPayment = (loanAmount * monthlyInterestRate) / (1 - Math.Pow(1 + monthlyInterestRate, -numberOfPayments));
-        double totalPayment = monthlyPayment * numberOfPayments;
-        double totalInterest = totalPayment - loanAmount;
-
-        // Display the result
-        Console.WriteLine($"Your estimated payment is {monthlyPayment:C}");
-        Console.WriteLine($"You paid {totalPayment:C} over the life of the loan");
-        Console.WriteLine($"Your total interest cost for the loan was {totalInterest:C}");
+    }
+    private static bool GetYesNoResponse(string prompt)
+    {
+        Console.Write(prompt);
+        string response = Console.ReadLine().Trim();
+        return response.ToUpper() == "Y";
     }
 }
